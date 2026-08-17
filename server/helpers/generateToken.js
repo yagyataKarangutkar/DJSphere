f56d3e1057ae0aebc0bafe0f56949f121a817b1c
yagyataKarangutkar <karangutkaryagyata@gmail.com>
2026-08-17 11:17:11 +0530

    doing changes

--- server/helpers/generateToken.js
+++ server/helpers/generateToken.js
@@ -23,9 +23,9 @@ const generateToken = (id, role) => {
 export const setTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,
+    sameSite: 'none',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
   res.cookie('token', token, cookieOptions);
